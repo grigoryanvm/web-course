05ad0a00d9a7f1910e0f3562832d5455856d77c5
grigoryanvm <grigoryanvm17@gmail.com>
2023-02-18 12:13:21 +0300

    вывод результата

--- zad1.php
+++ zad1.php
@@ -18,4 +18,24 @@ if ($_POST['mark']){
     $f=fopen($file,"w");
     fwrite($f,$mark);
     fclose($f);}
+
+    $f=fopen('5.txt',"r");
+    $mark=fread($f,1000);
+    fclose($f);
+    echo "Отлично-".$mark. "чел.<br>";
+
+    $f=fopen('4.txt',"r");
+    $mark=fread($f,1000);
+    fclose($f);
+    echo "Хорошо-" .$mark."чел.<br>";
+
+    $f=fopen('3.txt',"r");
+    $mark=fread($f,1000);
+    fclose($f);
+    echo "Удовлетворительно-".$mark."чел.<br>";
+
+    $f=fopen('2.txt',"r");
+    $mark=fread($f,1000);
+    fclose($f);
+    echo "Плохо-".$mark."чел.<br>";
 ?>
