626874eed22b70dc26bc4bde2ae88a70fd585f3d
grigoryanvm <grigoryanvm17@gmail.com>
2023-03-15 12:46:15 +0300

    primer

---
+++ PrimJs.js
@@ -0,0 +1,7 @@
+ let a="background-color:#00ffff; color:#ff00ff; " 
+a+="font-size:24pt; font-family:'Times New Roman'" 
+let naim='Сеть магазинов "ВСЁ ДЛЯ ДОМА"'
+let da= new Date()
+let d=da.getDate()+"."+(da.getMonth()+1)+"."+da.getFullYear() 
+document.write('<P align=center style= "'+a+'">'+
+naim+'</p><P>Сегодня '+d+'</p>')
