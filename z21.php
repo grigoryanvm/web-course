2414f96e57c867ef670b65364aed0820656afa0e
grigoryanvm <grigoryanvm17@gmail.com>
2023-03-31 19:28:08 +0300

    доп.пример

--- z21.php
+++ z21.php
@@ -15,4 +15,18 @@ $xml-> writeElement("total", "$1456");
 $xml-> endElement(); // закрытие элемента первого покупателя с ID = 2
 $xml-> endElement();
 echo $xml-> outputMemory();
+$xml2 = new XMLWriter();
+$xml2 -> openMemory();
+$xml2-> startDocument();
+$xml2-> startElement("products");
+$xml2-> writeElement("pid", 314);
+$xml2-> writeElement("name", "aplle");
+$xml2-> writeElement("prise", "$1.00");
+$xml2-> endElement();
+$xml2-> startElement("products");
+$xml2-> writeElement("pid", 315);
+$xml2-> writeElement("name", "mango");
+$xml2-> writeElement("prise", "$0.90");
+$xml2-> endElement();
+echo $xml2-> outputMemory();
 ?>
