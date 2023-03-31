d1d5d1b775485dd35e411dc8eee755c199129f0a
grigoryanvm <grigoryanvm17@gmail.com>
2023-03-31 19:31:35 +0300

    Чтение из файла

--- z1.php
+++ z1.php
@@ -9,3 +9,22 @@ $xml -> writeElement("name", "Марк"); // запись элемента
 $xml -> writeElement("address", "Санкт-Петербург");
 $xml -> endElement(); // закрытие корневого элемента
 echo $xml -> outputMemory(); // завершение записи в XML;
+// Чтение XML формата
+$rxml = new XMLReader(); // Создание элемента для чтения
+$rxml→xml($nXML); // Загрузка XML, $nXML – строка в формате
+// Переместиться к первому элементу customer 
+while($rxml→read() &&
+$rxml→name!== 'customer');
+$amountSpent = 0;
+// Получим значение поля total у второго узла дерева
+while($rxml→name === 'customer') {
+// Чтение текущего дочернего через SimpleXMLElement
+$node = new SimpleXMLElement($rxml→readOuterXML());
+// Проверяем, номер элемента, если он равен 2 то это искомый элемент
+if($node→id == 2) {
+$amountSpent = $node→total; break;
+}
+// Переместиться к следующему элементу customer
+$rxml→next('customer');
+}
+echo $amountSpent;
