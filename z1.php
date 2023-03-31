d62e0a7c9e24fd1cad5e351779c428bd1f065f73
grigoryanvm <grigoryanvm17@gmail.com>
2023-03-31 20:00:17 +0300

    чтение из файла

--- z1.php
+++ z1.php
@@ -11,20 +11,19 @@ $xml -> endElement(); // закрытие корневого элемента
 echo $xml -> outputMemory(); // завершение записи в XML;
 // Чтение XML формата
 $rxml = new XMLReader(); // Создание элемента для чтения
-$rxml→xml($nXML); // Загрузка XML, $nXML – строка в формате
+$rxml->xml($nXML); // Загрузка XML, $nXML – строка в формате
 // Переместиться к первому элементу customer 
-while($rxml→read() &&
-$rxml→name!== 'customer');
+while($rxml->read() && ($rxml->name!== 'Customer'));
 $amountSpent = 0;
 // Получим значение поля total у второго узла дерева
-while($rxml→name === 'customer') {
+while($rxml->name === 'Customer') {
 // Чтение текущего дочернего через SimpleXMLElement
 $node = new SimpleXMLElement($rxml→readOuterXML());
 // Проверяем, номер элемента, если он равен 2 то это искомый элемент
-if($node→id == 2) {
+if($node->id == 2) {
 $amountSpent = $node→total; break;
 }
 // Переместиться к следующему элементу customer
-$rxml→next('customer');
+$rxml->next('Customer');
 }
 echo $amountSpent;
--- z2.php
+++ z2.php
@@ -1,8 +1,16 @@
 <?php
-$xml = new DOMDocument();
+ $doc = new DOMDocument( );
+ $ele = $doc->createElement( 'Root' );
+ $ele->nodeValue = 'Hello XML World';
+ $doc->appendChild( $ele );
+ $doc->save("xml.txt");
+/*$xml = new DOMDocument();
 $xml_album = $xml->createElement("Album");
-$xml_track = $xml->createElement("Track");
-$xml_album->appendChild( $xml_track );
+$xml_track = $xml->writeElement("Track");
+$xml -> endElement();
 $xml->appendChild( $xml_album );
 $xml->save("xml.txt");
+*/
+$xml = simplexml_load_file("xml.txt") or die('Не удалось создать объект');
+  print_r($xml);
 ?>
